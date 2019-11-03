ec3a97ddf472d336f00149d07da66de12f7e08ea
Gil Gonçalves <lursty@gmail.com>
2019-11-03 17:47:11 +0000

    Some more playing around with the sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,45 +1,57 @@
 from dataclasses import dataclass
 import asyncio
 
-def handle_world(message, broker, db):
-    name = message.data["name"]
-    print(f"Hello {name}")
-
-    next_message = Message("quadrant:a", {"events": ["eagle"]})
-    broker.messages.append(next_message)
-
-def handle_quadrant(message, broker, db):
-    events = message.data["events"]
-    for e in events:
-        print(f"Something stir in quadrant A...")
-        print(f"Suddenly, {e} appears!")
-        survivors_in_quadrant = db["survivors"]
-        print(f"It startles {len(survivors_in_quadrant)} survivors.")
-
-
-class Broker:
-    def __init__(self):
-        self.messages = []
-
-HANDLERS = {
-    "world:all": handle_world,
-    "quadrant:a": handle_quadrant,
-}
-
 @dataclass
 class Message:
     _type: str
     data: dict
 
+def handle_world(message, broker):
+    name = message.data["name"]
+    print(f"Hello {name}")
+
+    next_message = Message("quadrant:a", {"events": ["eagle"]})
+    broker.add_message(next_message)
+
+def handle_quadrant(message, broker):
+    events = message.data["events"]
+    for e in events:
+        print(f"Something stir in quadrant A...")
+        print(f"Suddenly, {e} appears!")
+        survivors_in_quadrant = broker.db["survivors"]
+        print(f"It startles {len(survivors_in_quadrant)} survivors.")
+
+
+class Broker:
+
+    HANDLERS = {
+        "world:all": handle_world,
+        "quadrant:a": handle_quadrant,
+    }
+
+    def __init__(self, DB):
+        self.messages = []
+        self.db = DB
+        self.message_index = 0
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def work(self, loop):
+        if self.message_index < len(self.messages):
+            current_message = broker.messages[self.message_index]
+            loop.call_soon(self.HANDLERS[current_message._type](current_message, self), loop)
+            self.message_index += 1
+
+
+
 
 if __name__ == "__main__":
     DB = {"survivors": [1,1]}
-    broker = Broker()
-    broker.messages.append(Message("world:all", {"name": "world"}))
+    broker = Broker(DB)
+    broker.add_message(Message("world:all", {"name": "world"}))
 
     loop = asyncio.get_event_loop()
 
     while True:
-        if broker.messages:
-            message = broker.messages.pop()
-            loop.call_soon(HANDLERS[message._type](message, broker, DB), loop)
+        broker.work(loop)
